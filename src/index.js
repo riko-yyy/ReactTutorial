630f8965f8672db9b115394ff1319632e266907e
riko <c.c.3yegk1hass.w1s0vhd@gmail.com>
2020-08-03 23:58:28 +0900

    use for in Board render when Square rendering

--- src/index.js
+++ src/index.js
@@ -33,22 +33,16 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {" "}
-          {this.renderSquare(0)} {this.renderSquare(1)} {this.renderSquare(2)}{" "}
-        </div>{" "}
-        <div className="board-row">
-          {" "}
-          {this.renderSquare(3)} {this.renderSquare(4)} {this.renderSquare(5)}{" "}
-        </div>{" "}
-        <div className="board-row">
-          {" "}
-          {this.renderSquare(6)} {this.renderSquare(7)} {this.renderSquare(8)}{" "}
-        </div>{" "}
-      </div>
-    );
+    const boardSize = 3;
+    let rows = [];
+    for (let row = 0; row < boardSize; row++) {
+      let cols = [];
+      for (let col = 0; col < boardSize; col++) {
+        cols.push(this.renderSquare(boardSize * row + col));
+      }
+      rows.push(<div className="board-row"> {cols} </div>);
+    }
+    return <div>{rows}</div>;
   }
 }
 
